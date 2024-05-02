56161be83c44741484976829b3f7015ac7121395
balaji2107 <160450669+balaji2107@users.noreply.github.com>
2024-05-01 20:15:48 -1200

    Add files via upload

---
+++ LiftManagement/src/com/example/datalayer/LiftDatabase.java
@@ -0,0 +1,27 @@
+package com.example.datalayer;
+
+import com.example.model.Lift;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LiftDatabase {
+    public static LiftDatabase liftDatabase;
+    List<Lift> lifts=new ArrayList<>();
+
+    public static LiftDatabase getInstance(){
+        if(liftDatabase==null){
+            liftDatabase=new LiftDatabase();
+        }
+        return liftDatabase;
+    }
+
+    public List<Lift> getLifts(){
+        return lifts;
+    }
+    public void addLift(Lift lift){
+        lifts.add(lift);
+    }
+
+}
